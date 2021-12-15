f4afe0c67e7e8415a1c47c74383b131ecb38bcd9
michael wong <c14190168@john.petra.ac.id>
2021-12-14 22:50:18 -0800

    Perubahan

--- Travidec/Detailreportstatus.swift
+++ Travidec/Detailreportstatus.swift
@@ -11,7 +11,7 @@ import FirebaseFirestore
 
 class Detailreportstatus: UIViewController {
 
-    
+    let db = Firestore.firestore()
     @IBOutlet weak var Informername: UILabel!
     @IBOutlet weak var Tanggal: UILabel!
     @IBOutlet weak var Subject: UILabel!
@@ -22,14 +22,41 @@ class Detailreportstatus: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getData()
         // Do any additional setup after loading the view.
     }
     
     func getData() {
-        
-        
+        let docRef = db.document("reportData/FpB9XVSBGyvDDu6VhLgy")
+        docRef.getDocument { snapshot, error in
+            guard let data = snapshot?.data(), error == nil else {
+                return
+            }
+            
+            guard let tanggal = data["dateTime"] as? String else {
+                return
+            }
+            guard let desk = data["description"] as? String else {
+                return
+            }
+            guard let prio = data["priority"] as? String else {
+                return
+            }
+            guard let nama = data["name"] as? String else{
+                return
+            }
+            guard let sub = data["subject"] as? String else {
+                return
+            }
+            
+            self.Informername.text = nama
+            self.Tanggal.text = tanggal
+            self.Deskripsi.text = desk
+            self.Priority.text = prio
+            self.Subject.text = sub
+        }
     }
+    
 
     /*
     // MARK: - Navigation
